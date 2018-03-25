aef7cd7de7c529a2ab4028d7aefbcb3794698a90
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-03-26 02:03:58 +1000

    made flask app run with cors

--- server/src/server.py
+++ server/src/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_graphql import GraphQLView
 # from races import race_list, to_json_race_list
 from graphql_schema import schema
 
 app = Flask(__name__)
+CORS(app)
 
 app.add_url_rule(
     '/graphql',
