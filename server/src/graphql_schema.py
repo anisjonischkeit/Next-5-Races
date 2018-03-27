7b6df4060e3b34bd7b9fe9e1bb36557ae44b233b
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-03-28 02:23:30 +1000

    added abbility to get individual race

--- server/src/graphql_schema.py
+++ server/src/graphql_schema.py
@@ -14,7 +14,6 @@ race_list = sorted(race_list, key=lambda x: x.closing_time)
 
 class Query(graphene.ObjectType):
 		races = graphene.List(Race, limit=graphene.Int(required=True), only_open_races=graphene.Boolean())
-
 		def resolve_races(self, info, limit, only_open_races=False):
 				return_list = race_list
 				if only_open_races:
@@ -22,5 +21,9 @@ class Query(graphene.ObjectType):
 						return_list = list(filter(lambda race: race.closing_time > now, race_list))
 				
 				return return_list[:limit]
+
+		race = graphene.Field(Race, id=graphene.String(required=True))
+		def resolve_race(self, info, id):
+				return next(filter(lambda race: race.id == id, race_list), None)
 
 schema = graphene.Schema(query=Query)
